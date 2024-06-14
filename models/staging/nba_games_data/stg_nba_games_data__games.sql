f1e43024f60cf94718c55b8b3b889596646228dd
jesus montes <kexuia@hotmail.com>
2024-06-14 16:40:58 +0000

    stg_games casteada

--- models/staging/nba_games_data/stg_nba_games_data__games.sql
+++ models/staging/nba_games_data/stg_nba_games_data__games.sql
@@ -10,29 +10,41 @@ renamed as (
 
     select
         --_fivetran_id,
-        game_id,
-        game_date_est,
-        home_team_id,
-        team_id_home,
-        team_id_away,
-        visitor_team_id,
-        pts_home,
-        pts_away,
-        reb_home,
-        ast_home,
-        fg_pct_home,
-        fg3_pct_home,
-        ft_pct_home,
-        home_team_wins,
-        reb_away,
-        ast_away,
-        fg_pct_away,
-        fg3_pct_away,
-        ft_pct_away,
+        game_id
+        ,game_date_est
+        ,home_team_id
+        ,team_id_home
+        ,team_id_away
+        ,visitor_team_id
+        ,{{round_and_alias('pts_home')}}
+        --,pts_home
+        ,{{round_and_alias('pts_away')}}
+        --,pts_away
+        ,{{round_and_alias('reb_home')}}
+        --,reb_home
+        ,{{round_and_alias('ast_home')}}
+        --,ast_home
+        --,fg_pct_home
+        ,{{round_and_alias('fg_pct_home',2)}} 
+        --,fg3_pct_home
+        ,{{round_and_alias('fg3_pct_home',2)}} 
+        --,ft_pct_home
+        ,{{round_and_alias('ft_pct_home',2)}} 
+        ,home_team_wins
+        ,{{round_and_alias('reb_away')}}
+        --,reb_away
+        ,{{round_and_alias('ast_away')}}       
+        --,ast_away
+        ,{{round_and_alias('fg_pct_away',2)}} 
+        --,fg_pct_away
+        ,{{round_and_alias('fg3_pct_away',2)}} 
+        --,fg3_pct_away
+        ,{{round_and_alias('ft_pct_away',2)}} 
+        --,ft_pct_away
         --game_status_text,
-        season,
+        ,season
         --_fivetran_deleted,
-        _fivetran_synced
+        ,_fivetran_synced
 
     from source
 
